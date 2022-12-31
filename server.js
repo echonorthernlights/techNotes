18dec7ba44576d842a3c7bcd85a3a7c6db253d1e
echonorthernlights <elhassanianas@gmail.com>
2022-12-31 17:07:01 +0100

    CORS fix

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cookieParser = require("cookie-parser");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const corsOptions = require("./config/corsOptions");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -10,7 +21,16 @@ app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
   res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "Not Found !!" });
+  } else {
+    res.type("txt").send("Not Found !!!");
+  }
 });
+
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on PORT : ${PORT} ....`);
 });
